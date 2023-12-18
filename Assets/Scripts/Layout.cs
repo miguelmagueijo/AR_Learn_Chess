8050a69551b523914a0bce158705a4eb288df3fd
AndreFarinha <farinha0022@gmail.com>
2023-12-18 11:33:45 +0000

    Menus

---
+++ Assets/Scripts/Layout.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Layout : MonoBehaviour
+{
+     public void ChamaCena()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Layout");
+    }
+}
---
+++ Assets/Scripts/XequeMate.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XequeMate : MonoBehaviour
+{
+    public void ChamaCena()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Xeque_Mate");
+    }
+}
